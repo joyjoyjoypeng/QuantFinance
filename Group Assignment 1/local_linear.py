3c2b4452639d030dfce1251dd8e7eca93c7c75ed
whoisvasu <vasu@u.yale-nus.edu.sg>
2022-02-06 10:50:41 +0800

    Updated stuff

--- Group Assignment 1/local_linear.py
+++ Group Assignment 1/local_linear.py
@@ -6,18 +6,20 @@ you may assume that a Gaussian kernel should be used.
 
 You should also provide functions for predicting new values and plotting the estimated regression
 function. The required inputs to your program would be 2 files with n rows and 1 column of
-numeric data – one containing the x variable and the other containing the y variable – an output
+numeric data - one containing the x variable and the other containing the y variable - an output
 file path, and the number of folds. So at it's very basic level, the program could be invoked
 as follows:
 
-python local_linear.py
---x xin.dms –-y yin.dms –-output output.dms –-num_folds 10 --plot true --xout xout.dms
+python local_linear.py --x xin.dms --y yin.dms --output output.dms --num_folds 10 
+
+Optional arguments:
+--plot True --xout xout.dms
 
 And in output, you would write a n x 1 column of numeric data containing the values of the fitted
 function evaluated at the points found in xin.
 
-Your program should also support the optional arguments –-plot, which would display a scatter plot
-of y against x with the fitted function drawn over it, as well as –-xout, to which the path to a
+Your program should also support the optional arguments --plot, which would display a scatter plot
+of y against x with the fitted function drawn over it, as well as --xout, to which the path to a
 further n x 1 file of numeric data would be provided so that output would contains the values of
 the fitted function evaluated at these points rather than the training points found in xin.
 '''
@@ -27,18 +29,36 @@ import math
 import numpy as np
 from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
+
 def file_import(file):
     '''
-    Imports a file and stores it as an array
+    This function imports a file and stores its contents in an array
+
+    Inputs: 
+        file (path): The file to be imported
+
+    Returns:
+        inputs (array): Array containing the values in the file
     '''
+
     with open(file,'r') as vals:
         inputs= np.array(list(map(lambda f: float(f.strip()), vals.readlines())))
     return inputs
 
 def get_weight(x_training, x_actual, bandwidth_value):
     '''
-    Gets weight
+    This function returns the weight of the x_actual in estimating the y value of the x_training,
+    given a specific bandwitdth_value
+
+    Inputs: 
+        x_training (float): The x value that is being trained
+        x_actual (float): The x value whose weight is being computed 
+        bandwidth_value (float): The bandwidth being tested in a run
+
+    Returns:
+        weight_k (float): The k weight of the x_actual
     '''
+
     weight_k = math.exp(-((x_training - x_actual) ** 2) / bandwidth_value)
     return weight_k
 
@@ -46,8 +66,17 @@ bandwidth_values = np.linspace(0.1, 0.2, 2)
 
 def get_lowest_h(x_train, y_train):
     '''
-    This gives us the lowest h for a particular training set in a fold
+    This function tests all the possible bandwidth values and returns the one that produces the lowest
+    MSE for a particular training set in a fold
+
+    Inputs: 
+        x_train (array): The x values being trained in a run
+        y_train (array): The actual y values for the x values being trained
+
+    Returns:
+        lowest_h (float): The h value that caused the lowest MSE to be obtained
     '''
+
     mse_container = {}
 
     for h_val in bandwidth_values:
@@ -70,14 +99,25 @@ def get_lowest_h(x_train, y_train):
 
         mse = sum(errors)/len(errors)
         mse_container[h_val] = mse
+        lowest_h = min(mse_container, key=mse_container.get)
 
-    return min(mse_container, key=mse_container.get)
+    return lowest_h
 
 def for_testing(x_test, y_test, h_val):
     '''
-    This function is used to test the h with the lowest MSE in the training phase
-    against the testing set
+    This function is used to test the h value that had returned the lowest MSE during 
+    the training phase against the x and y values in the testing set
+    
+    Inputs: 
+        x_test (array): The x values being tested in a run
+        y_test (array): The actual y values for the x values being tested
+        h_val (float): The h value that had caused the lowest MSE to be obtained
+
+    Returns:
+        test_output (list): The h value that caused the lowest MSE to be obtained,
+        as well as the MSE obtained by using that h value
     '''
+
     errors = []
     for (z_index, z_val) in enumerate(x_test):
         k_vals = []
@@ -96,13 +136,24 @@ def for_testing(x_test, y_test, h_val):
         errors.append(error ** 2)
 
     mse = sum(errors)/len(errors)
-    return [h_val, mse]
+    test_output = [h_val, mse]
+    
+    return test_output
 
 def get_optimal_h(the_x, the_y, k_folds):
     '''
-    Combines the previous two functions to obtain an optimal h
-    across all the folds
+    This function combines the previous two functions to obtain an optimal h
+    across all the training and testing phases of the folds
+    
+    Inputs: 
+        the_x (array): The entire set of x values
+        the_y (array): The entire set of y values
+        k_folds (int): The number of folds to be used for cross validation
+
+    Returns:
+        test_output (list): The h value that caused the lowest MSE to be obtained
     '''
+
     kf_creator = KFold(n_splits=k_folds, shuffle=True, random_state=np.random)
     tests = []
     counter = 0
@@ -115,15 +166,24 @@ def get_optimal_h(the_x, the_y, k_folds):
         h_val = get_lowest_h(x_train, y_train)
         result = for_testing(x_test, y_test, h_val)
         tests.append(result)
+
     print('Training Completed! :)')
-    best_h = min(tests, key = lambda x: x[1])
+    best_h = min(tests, key = lambda x: x[1])[0]
     #print(tests)
-    return best_h[0]
+    return best_h
 
 def actual_prediction(x_set, y_set, h_val):
     '''
-    Conducts the actual prediction on the x_set using the other
-    y_set values and returns the predictions as an array
+    This function conducts the actual prediction on the x_set using the other
+    y_set values and returns the predictions 
+    
+    Inputs: 
+        x_set (array): The set of x values for which predictions need to be conducted
+        y_set (array): The set of y values that will be used for predictions
+        h_val (float): The optimal h that was found during the cross validation phase
+
+    Returns:
+        y_hats (list): The predictions of the y values in a list
     '''
     y_hats = []
     for (z_index, z_val) in enumerate(x_set):
@@ -145,18 +205,37 @@ def actual_prediction(x_set, y_set, h_val):
 
 def create_output(pred, output):
     '''
-    Creates a new file given the predicted y values
+    This function reates a new file given the predicted y values
+    
+    Inputs: 
+        pred (array): The list of predicted y values
+        output (path): The path for the output file to be created
+
+    Returns:
+        output (path): The path to the output file (For function testing purposes)
     '''
+
     with open(output, 'w') as new_file:
         for i in range(len(pred)-1):
             new_file.write(str(pred[i]) + "\n")
         new_file.write(str(pred[-1]))
+
     return output
 
 def create_plot(x_vals, y_vals, pred):
     '''
-    Creates a plot
+    This function creates a plot showcasing the y_vals and predicted
+    y values against the x_vals
+    
+    Inputs: 
+        x_vals (array): The array of x values
+        y_vals (array): The array of y values
+        pred (list): The list of predicted y values
+
+    Returns:
+        --Nothing is returned as the plot will directly be created and saved in the folder--
     '''
+
     plt.scatter(x_vals, y_vals, color = 'lightgreen', s=30, alpha = 0.8,
                 marker = 'o', label = "Actual Y")
     plt.scatter(x_vals, pred, color = 'red', s=30, alpha = 0.3,
@@ -168,8 +247,18 @@ def create_plot(x_vals, y_vals, pred):
 
 def main (xin, yin, output, k_folds, plot = None, xout = None):
     '''
-    Main
-    return something for easier testing
+    This is the main function containing all the others
+    
+    Inputs: 
+        xin (path): The path to the original x values
+        yin (path): The path to the original y values
+        output (path): The path foe the output file to be created
+        k_folds (int): The number of folds to be used in the cross validation
+        plot (bool): The optional command for whether a plot is to be generated
+        xout (path): The optional path to a new set of x_values for prediction
+
+    Returns:
+        output (path): The path to the output file (For function testing purposes)
     '''
     x_inputs = file_import(xin)
     y_inputs = file_import(yin)
--- Group Assignment 1/test.py
+++ Group Assignment 1/test.py
@@ -11,6 +11,13 @@ Output_file = local_linear.main("xin.dms","yin.dms","output.dms",3)
 def test_main_arguments():
     '''
     This function tests whether invalid arguments raise an error
+
+    Inputs: 
+        -- No inputs are required as this function assumes an error will be raised
+        when insufficient arguments are passed to the main function -- 
+
+    Returns:
+        -- Nothing should be returned ideally if only the predicted errors are raised --
     '''
     with pytest.raises(TypeError):
         local_linear.main("xin.dms","yin.dms","output.dms")
@@ -22,6 +29,14 @@ def test_main_arguments():
 def is_float(data):
     '''
     This function is used to test whether all values in a data set are floats
+    
+    Inputs: 
+        data (array): An array of values that are to be tested
+
+    Returns:
+        -- A bool will be returned in the technical sense, "True" if all the values in the
+        data set are indeed floats, "False" + an error statement if a particular value is
+        flagged out to be not a float --
     '''
     for data_val in data:
         assert isinstance(data_val, float), "the input {0} is not a float".format(data_val)
@@ -29,6 +44,14 @@ def is_float(data):
 def test_import_float():
     '''
     This function is used to test whether all values in the imported data set are floats
+    
+    Inputs: 
+        -- No inputs are required as this function uses the arrays defined 
+        earlier in the file --
+
+    Returns:
+        -- Nothing should be returned ideally if both arrays of x and y values
+        only contain floats --
     '''
     is_float(X_raw)
     is_float(Y_raw)
@@ -36,12 +59,30 @@ def test_import_float():
 def test_import_lenght():
     '''
     This function is used to test whether the imported data sets have matching length
+    
+    Inputs: 
+        -- No inputs are required as this function uses the arrays defined 
+        earlier in the file --
+
+    Returns:
+        -- Nothing should be returned ideally if both arrays of x and y values
+        are of the same length --
     '''
     assert len(X_raw) == len (Y_raw), "data length are not equal for xin and yin"
 
 def test_get_weight():
     '''
     This function is used to test whether the get_weight() function operates as expected
+    
+    Inputs: 
+        -- No inputs are required as this function internally creates scenarios 
+        to be tested --
+
+    Returns:
+        -- A bool will be returned in the technical sense, "True" if all the equalities happen
+        to be true, indicating that the "get_weight()" function works exactly like it is
+        supposed to, "False" if an equality doesn't match up or if expected error does not
+        get raised --
     '''
     assert local_linear.get_weight(1.5, 1.4, 0.2) == math.exp(-((1.5 - 1.4) ** 2) / 0.2)
     assert local_linear.get_weight(1, 1, 0.5) == 1
@@ -54,6 +95,15 @@ def test_main_output():
     '''
     This function is used to test whether the output data set is consisted of floats
     and has a matching length as the input data sets.
+    
+    Inputs: 
+        -- No inputs are required as this function internally creates scenarios
+        to be tested --
+
+    Returns:
+        -- A bool will be returned in the technical sense, "True" if all the values in the
+        final output are indeed floats and are of the same length as the original sets of
+        x and y values, "False" if any of the above conditions are not met --
     '''
     output_raw = local_linear.file_import(Output_file)
     is_float(output_raw)
